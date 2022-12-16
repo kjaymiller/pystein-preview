d04ef2b6b76c1c00c10a26721c84ec2c2803acd6
Jay Miller <kjaymiller@gmail.com>
2022-12-16 11:06:02 -0800

    fix minor spacing issue

--- function_app.py
+++ function_app.py
@@ -10,10 +10,10 @@ app = func.FunctionApp()
 @app.function_name(name="HttpTrigger1")
 @app.route(route="hello")
 def test_function(req: func.HttpRequest) -> func.HttpResponse:
-     logging.info('Python HTTP trigger function processed a request.')
+    logging.info('Python HTTP trigger function processed a request.')
 
-     name = req.params.get('name')
-     if not name:
+    name = req.params.get('name')
+    if not name:
         try:
             req_body = req.get_json()
         except ValueError:
@@ -21,10 +21,10 @@ def test_function(req: func.HttpRequest) -> func.HttpResponse:
         else:
             name = req_body.get('name')
 
-     if name:
+    if name:
         return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-     else:
+    else:
         return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
+            "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+            status_code=200
         )
